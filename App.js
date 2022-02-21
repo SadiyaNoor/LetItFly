85d9b077229fab47d3e89bfcacb8f58a56ca4947
Sadiya Noor <sadiya@email.unc.edu>
2022-02-21 06:24:58 -0500

    commit

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
+import * as React from 'react';
+
+const renderTime = (dimension, time) => {
+  return (
+    <div className="time-wrapper">
+      <div className="time">{time}</div>
+      <div>{dimension}</div>
+    </div>
+  );
+};
+
+
 
 export default function App() {
+  const [isPlaying, setIsPlaying] = React.useState(true)
+  const [breathState, setBreathState] = React.useState(true)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+      <TouchableOpacity
+        onPress={() => setIsPlaying(prev => !prev)}
+        style={styles.button}>
+        <Text>Balloon</Text>
+      </TouchableOpacity>
+      <Text style={styles.breathstate}> Inhale
+      </Text>
+      <CountdownCircleTimer
+        isPlaying={isPlaying}
+        duration={4}
+        colors={["#D5CE58"]}
+        colorsTime={[4]}
+        onComplete={() => ({ shouldRepeat: true, delay: 2 })}
+    >
+      {({ remainingTime, color }) => (
+        <Text style={{ color, fontSize:35, textAlign: 'center'}}>
+          {renderTime('', remainingTime)}
+        </Text>
+      )}
+    </CountdownCircleTimer>
+    
+    
+  </View>
+  )
 }
 
 const styles = StyleSheet.create({
+  button: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    backgroundColor: '#D5CE58',
+  },
+  breathstate: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: 170,
+    fontSize: 30,
+    color: "#FFFFFF",
+    
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#104210',
+    padding: 25,
   },
+  
+  
 });
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # LetItFly
+
+Let it Fly is an app created for those feeling overwhelmed at times.
+The app functions as a guided breathing exercise with a timer that times the exhales and inhales of the user, inflating a visual balloon at the same time.
+
+The app is coded in JavaScript using react native and live tested using an android simulator. We were also able to download Expo and see the app change in real time on our mobile phones.
+
+This is my first project at a Hackathon and I had so much fun coding in a new language. I entered not expecting to create anything as my mind was filled with self doubt but with the encouragement of the sponsors and team at PearlHacks I was able to try and create something I am passionate about.
